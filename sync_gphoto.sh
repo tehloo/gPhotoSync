c416dce51e2e817129775af2b7c56bbdf4e39932
JC Kim <jinchul.kim@lge.com>
2020-01-07 17:15:27 +0900

    Make sync.log simple to read

--- sync_gphoto.sh
+++ sync_gphoto.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
+echo
+echo "Starting GPhotoSync...."
+date
+echo
+
 cd /home/jckim/works/1226_gPhotos
 . .venv/bin/activate
 
-echo "Log starts " >> sync.log
-date >> sync.log
-
-nohup python3 -u gphotoSync.py >> sync.log 2>&1
-nohup python3 -u imageConverter.py >> sync.log 2>&1
+python3 gphotoSync.py
+python3 imageConverter.py
